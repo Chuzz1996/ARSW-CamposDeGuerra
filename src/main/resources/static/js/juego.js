b0de2b1aed1b83545e477a6ab8468040002e6ae4
martinjhm271 <martinjhm271@hotmail.es>
2017-10-17 23:29:07 -0500

    Backend Maquina

--- src/main/resources/static/js/juego.js
+++ src/main/resources/static/js/juego.js
@@ -1,34 +1,38 @@
-var juego = function(){
-    
+var juego = function () {
+
     var myGamePiece;
 
     var directionImageTank = "/images/tank";
     var directionImageShoot = "/images/bullet";
-    var directionShoot = 4; 
-    
+    var directionShoot = 4;
+
     var myGameArea = {
-        canvas : document.createElement("canvas"),
-        start : function() {
-            this.canvas.width = 500;
-            this.canvas.height = 500;
+        canvas: document.createElement("canvas"),
+        start: function () {
+            var w = window.innerWidth;
+            var h = window.innerHeight;
+            this.canvas.width = w-200;
+            this.canvas.height = h-200;
             this.context = this.canvas.getContext("2d");
-            document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+            this.context.fillStyle = "blue";
+            this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+            document.getElementById("Game").appendChild(this.canvas);
             this.interval = setInterval(updateGameArea, 1);
             window.addEventListener('keydown', function (e) {
                 myGameArea.key = e.keyCode;
-            })
+            });
             window.addEventListener('keyup', function (e) {
                 myGameArea.key = false;
-            })
-        }, 
-        clear : function(){
+            });
+        },
+        clear: function () {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         }
-    }
+    };
 
     function component(width, height, color, x, y, type) {
         this.gamearea = myGameArea;
-        if(type=="image"){
+        if (type === "image") {
             this.image = new Image();
             this.image.src = color;
         }
@@ -40,89 +44,111 @@ var juego = function(){
         this.x = x;
         this.y = y;
         this.shoots = [];
-        this.update = function() {
+        this.update = function () {
             ctx = myGameArea.context;
-            if(type=="image"){
-                ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
-            }else{
+            if (type === "image") {
+                ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            } else {
                 ctx.fillStyle = color;
                 ctx.fillRect(this.x, this.y, this.width, this.height);
             }
-            for(let i = 0; i < this.shoots.length; i++){
-                this.shoots[i].update;
-                console.info(this.shoots[i].x+","+this.shoots[i].y);
+            for (let i = 0; i < this.shoots.length; i++) {
+                ctx.drawImage(this.shoots[i].image, this.shoots[i].x, this.shoots[i].y, this.shoots[i].width, this.shoots[i].height);
+                if (this.shoots[i].dir === 4) {
+                    this.shoots[i].x += 1;
+                } else if (this.shoots[i].dir === 3) {
+                    this.shoots[i].x -= 1;
+                } else if (this.shoots[i].dir === 2) {
+                    this.shoots[i].y -= 1;
+                } else {
+                    this.shoots[i].y += 1;
+                }
             }
-        }
-        this.newPos = function() {
+        };
+        this.newPos = function () {
             this.x += this.speedX;
-            this.y += this.speedY;        
-        }
+            this.y += this.speedY;
+        };
     }
-    
-    function bullet(width, height, color, x, y, type){
+
+    function bullet(width, height, color, x, y, type, dir) {
         this.gamearea = myGameArea;
-        if(type=="image"){
+        if (type === "image") {
             this.image = new Image();
             this.image.src = color;
         }
+        this.dir = dir;
         this.width = width;
         this.height = height;
-        this.speedX = 0;
-        this.speedY = 0;
         this.x = x;
         this.y = y;
-        this.update = function() {
-            ctx = myGameArea.context;
-            console.info("ENTRO");
-            if(type=="image"){
-                ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
-            }else{
-                ctx.fillStyle = color;
-                ctx.fillRect(this.x, this.y, this.width, this.height);
-            }
-        }
-        this.newPos = function() {
-            this.x += this.speedX;
-            this.y += this.speedY;        
-        }
     }
-    
+
     function updateGameArea() {
         myGameArea.clear();
+        myGameArea.context.fillStyle = "#A9A9A9";
+        myGameArea.context.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
         myGamePiece.speedX = 0;
-        myGamePiece.speedY = 0;    
-        
-        if (myGameArea.key && myGameArea.key == 37) {
-            myGamePiece.speedX = -myGamePiece.speed; 
-            myGamePiece.image.src = directionImageTank+"2.png";
+        myGamePiece.speedY = 0;
+
+        //THE A KEY
+        if (myGameArea.key && myGameArea.key === 65) {
+            myGamePiece.speedX = -myGamePiece.speed;
+            myGamePiece.image.src = directionImageTank + "2.png";
             directionShoot = 3;
         }
-        if (myGameArea.key && myGameArea.key == 39) {
-            myGamePiece.speedX = myGamePiece.speed; 
-            myGamePiece.image.src = directionImageTank+"1.png";
+        //THE D KEY
+        if (myGameArea.key && myGameArea.key === 68) {
+            myGamePiece.speedX = myGamePiece.speed;
+            myGamePiece.image.src = directionImageTank + "1.png";
             directionShoot = 4;
         }
-        if (myGameArea.key && myGameArea.key == 38) {
-            myGamePiece.speedY = -myGamePiece.speed; 
-            myGamePiece.image.src = directionImageTank+"4.png";
+        //THE W KEY
+        if (myGameArea.key && myGameArea.key === 87) {
+            myGamePiece.speedY = -myGamePiece.speed;
+            myGamePiece.image.src = directionImageTank + "4.png";
             directionShoot = 2;
         }
-        if (myGameArea.key && myGameArea.key == 40) {
-            myGamePiece.speedY = myGamePiece.speed; 
-            myGamePiece.image.src = directionImageTank+"3.png";
+        //THE S KEY
+        if (myGameArea.key && myGameArea.key === 83) {
+            myGamePiece.speedY = myGamePiece.speed;
+            myGamePiece.image.src = directionImageTank + "3.png";
             directionShoot = 1;
         }
-        myGamePiece.newPos();    
-        if(myGameArea.key && myGameArea.key == 32){
-            myGamePiece.shoots.push(new bullet(myGamePiece.x+1,myGamePiece.y+1,directionImageShoot+directionShoot+".png",10,20,"image"));
+        //THE SPACE KEY   
+        if (myGameArea.key && myGameArea.key === 32) {
+            var h, w,sx,sy;
+            if (directionShoot === 3) {
+                h = 10;
+                w = 30;
+                sx=-5;
+                sy=10;
+            } else if (directionShoot === 4) {
+                h = 10;
+                w = 30;
+                sx=5;
+                sy=10;
+            } else if (directionShoot === 2) {
+                h = 30;
+                w = 10;
+                sx=10;
+                sy=-10;
+            } else {
+                h = 30;
+                w = 10;
+                sx=10;
+                sy=10;
+            }
+            myGamePiece.shoots.push(new bullet(w, h, directionImageShoot + directionShoot + ".png", myGamePiece.x+sx, myGamePiece.y+sy, "image", directionShoot));
         }
+        myGamePiece.newPos();
         myGamePiece.update();
     }
-    
+
     return{
-        movimiento:function(){
+        movimiento: function () {
             myGameArea.start();
-            myGamePiece = new component(30, 30, directionImageTank+"1.png", 10, 120,"image");
+            myGamePiece = new component(30, 30, directionImageTank + "1.png", 10, 120, "image");
         }
-    }
+    };
 }();
