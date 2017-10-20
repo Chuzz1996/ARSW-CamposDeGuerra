d3c70a2fa83069e55b5ff977f6c636e29025edc3
cristianpinzon97 <cristian.pinzon@mail.escuelaing.edu.co>
2017-10-19 23:42:02 -0500

    Manejo del tiempo en sala y de espera, y vida

--- src/main/java/edu/eci/arsw/camposdeguerra/persistence/impl/InMemoryCamposDeGuerraUsuarioPersistence.java
+++ src/main/java/edu/eci/arsw/camposdeguerra/persistence/impl/InMemoryCamposDeGuerraUsuarioPersistence.java
@@ -23,12 +23,14 @@ public class InMemoryCamposDeGuerraUsuarioPersistence implements CamposDeGuerraU
     
     
     public InMemoryCamposDeGuerraUsuarioPersistence() {
-        Usuario u1 = new Usuario("martin", null, 99999,100);
-        Usuario u2= new Usuario("cristian", null, 0,100);
+        Usuario u1 = new Usuario("martin", null, 99,100);
+        Usuario u2= new Usuario("cristian", null, 100,100);
+        Usuario u4= new Usuario("NMCC", null, 8888,100);
         Usuario u3 = new Usuario("felipe", null, 123,100);
         users.putIfAbsent("martin", u1);
         users.putIfAbsent("cristian", u2);
         users.putIfAbsent("felipe", u3);
+        users.putIfAbsent("NMCC", u4);
     }
     
     @Override
